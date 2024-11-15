c57029b9d89e353a46510a14909833977002a52e
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-15 05:01:55 -0500

    252. Inicio de proyecto - MapsApp

---
+++ src/presentation/screens/maps/MapScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export const MapScreen = () => {
+	return (
+		<View>
+			<Text>MapScreen</Text>
+		</View>
+	);
+};
---
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export const PermissionsScreen = () => {
+	return (
+		<View>
+			<Text>PermissionsScreen</Text>
+		</View>
+	);
+};
