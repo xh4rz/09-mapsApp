c629afc0599ee8d97405c19a781c9a5de828501e
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-18 22:24:39 -0500

    254. Rutas y StackNavigator

---
+++ src/MapsApp.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './presentation/navigation/StackNavigator';
+
+export const MapsApp = () => {
+	return (
+		<NavigationContainer>
+			<StackNavigator />
+		</NavigationContainer>
+	);
+};
---
+++ src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,29 @@
+import { LoadingScreen } from '../screens/loading/LoadingScreen';
+import { PermissionsScreen } from '../screens/permissions/PermissionsScreen';
+import { MapScreen } from '../screens/maps/MapScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export type RootStackParams = {
+	LoadingScreen: undefined;
+	PermissionsScreen: undefined;
+	MapScreen: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+	return (
+		<Stack.Navigator
+			initialRouteName="LoadingScreen"
+			screenOptions={{
+				headerShown: false,
+				cardStyle: {
+					backgroundColor: 'white'
+				}
+			}}>
+			<Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+			<Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+			<Stack.Screen name="MapScreen" component={MapScreen} />
+		</Stack.Navigator>
+	);
+};
