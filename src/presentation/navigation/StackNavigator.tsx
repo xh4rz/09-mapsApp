759873e4e771de5363c2eb6729d5b0720f19e930
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-20 02:16:59 -0500

    260. Consumir nuestro Store

--- src/presentation/navigation/StackNavigator.tsx
+++ src/presentation/navigation/StackNavigator.tsx
@@ -14,7 +14,7 @@ const Stack = createStackNavigator<RootStackParams>();
 export const StackNavigator = () => {
 	return (
 		<Stack.Navigator
-			initialRouteName="LoadingScreen"
+			initialRouteName="PermissionsScreen"
 			screenOptions={{
 				headerShown: false,
 				cardStyle: {
--- src/presentation/screens/permissions/PermissionsScreen.tsx
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
+import { globalStyles } from '../../../config/theme/styles';
+import { usePermissionStore } from '../../store/permissions/usePermissionStore';
 
 export const PermissionsScreen = () => {
+	const { locationStatus, requestLocationPermission } = usePermissionStore();
 	return (
-		<View>
-			<Text>PermissionsScreen</Text>
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Habilitar Ubicación</Text>
+
+			<Pressable
+				style={globalStyles.btnPrimary}
+				onPress={requestLocationPermission}>
+				<Text style={{ color: 'white' }}>Habilitar Localización</Text>
+			</Pressable>
+
+			<Text>Estado actual: {locationStatus}</Text>
 		</View>
 	);
 };
